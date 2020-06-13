8f7e2d3803ecb241eefad5dcbfd0eff0b2a4ebaa
Chief <davlatov.yusufxon@gmail.com>
2020-06-13 15:56:53 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+//https://discord.com/api/oauth2/authorize?client_id=721343847711506464&permissions=8&scope=bot **invite link of the bot**
+//NzIxMzQzODQ3NzExNTA2NDY0.XuTJ1g.BHkR5Y9Fe5-mSeFQVa_TmQOCoaY **bot's token**
+
